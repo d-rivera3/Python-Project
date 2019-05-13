e8f96aa92605f11894d4f868b68fa66385583440
Daniel Rivera <drivera3@uccs.edu>
2019-05-12 21:49:28 -0600

    text change

--- Tetris/Tetris.py
+++ Tetris/Tetris.py
@@ -162,7 +162,7 @@ def main():
     DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
     BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
     BIGFONT = pygame.font.Font('freesansbold.ttf', 100)
-    pygame.display.set_caption('Tetromino')
+    pygame.display.set_caption('Tetris')
 
     showTextScreen('Tetromino')
     while True: # game loop
